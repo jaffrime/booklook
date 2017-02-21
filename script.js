63b427680a8b242da24776a46a99a27c16020226
Julian Affrime <julian.affrime@gmail.com>
2017-02-21 17:12:07 +0200

    problem with array input

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $('.search').on('click',function(){
-  var isbnVar = $('.isbn-input').val();
-  // alert(isbnVar);
-  fetch(isbnVar);
+  var searchData = {
+    searchVal: $(this).closest('div').find('input').val(),
+    searchType: $(this).closest('div').find('input').data().search
+  }
+  // console.log(searchData);
+  fetch(searchData);
 });
 
 $(document).ajaxStart(function(){
@@ -12,16 +15,66 @@ $(document).ajaxComplete(function(){
     $("#loadingimage").css("display", "none");
 });
 
-var fetch = function (isbnVar) {
+var fetch = function (searchData) {
+  var urlVal;
+
+  switch (searchData.searchType) {
+    case "isbn":
+      urlVal = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + searchData.searchVal;
+      break;
+
+    case "title":
+      urlVal = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + searchData.searchVal;
+      break;
+
+    case "author":
+      urlVal = "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + searchData.searchVal;
+      break;
+
+    default:
+  }
+
   $("loadingimage").show();
   $.ajax({
     method: "GET",
-    url: "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbnVar,
+    url: urlVal,
     dataType: "json",
     success: function(data) {
       $("#loadingimage").hide();
-      //debugger;
-      //console.log(data);
+      // debugger;
+      // console.log(data);
+      if (data.totalItems > 1) {
+      //  displayOptions(data);
+        var source = $("#options-template").html();
+        var template = Handlebars.compile(source);
+        var bookOptions = {
+          list: [
+            //{title: , author: }
+          ]
+        };
+        // console.log(data.items[0].volumeInfo.title);
+        // console.log(data.items[0].volumeInfo.authors[0]);
+        // bookOptions.list.push({title: data.items[0].volumeInfo.title, author: data.items[0].volumeInfo.authors[0]});
+        // console.log(data.items[1].volumeInfo.title);
+        // console.log(data.items[1].volumeInfo.authors[0]);
+        // bookOptions.list.push({title: data.items[1].volumeInfo.title, author: data.items[1].volumeInfo.authors[0]});
+        // console.log(data.items[2].volumeInfo.title);
+        // console.log(data.items[2].volumeInfo.authors[0]);
+        // bookOptions.list.push({title: data.items[2].volumeInfo.title, author: data.items[2].volumeInfo.authors[0]});
+
+
+        for (i=0; i<data.items; i++) {
+          console.log(data.items[i].volumeInfo.title);
+          console.log(data.items[i].volumeInfo.authors[0]);
+          bookOptions.list.push({title: data.items[i].volumeInfo.title, author: data.items[i].volumeInfo.authors[0]});
+        }
+        console.log(bookOptions);
+        $(".data-presentation").empty();
+        var newHTML = template(bookOptions);
+        $(".data-presentation").append(newHTML);
+
+      } else {
+      //  displayBook(data);
       var source = $("#book-template").html();
       var template = Handlebars.compile(source);
       var bookData = {
@@ -35,9 +88,19 @@ var fetch = function (isbnVar) {
       var newHTML = template(bookData);
       $(".data-presentation").append(newHTML);
 
+      }
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
     }
   });
+};
+
+var displayBook = function (data) {
+
+
+};
+
+var displayOptions = function (data) {
+
 };
